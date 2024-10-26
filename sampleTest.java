962c6b7be6950dda2775957e2fb30fc2f1a8b5a0
Ronny <ronny@gmail.com>
2024-10-26 13:16:54 +0530

    added one more file

--- sampleTest.java
+++ sampleTest.java
@@ -4,5 +4,6 @@ System.out.println("Hello world !");
 System.out.println("HI..From Jenkins Project");
 System.out.println("Output from jenkins project");
 System.out.println("check");
+System.out.println("test for compile and run");
 }
 }
