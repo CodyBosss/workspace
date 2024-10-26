eeaf89a2402c956ce01a1eac856c8e87e8aefa14
Ronny <ronny@gmail.com>
2024-10-26 12:40:50 +0530

    Initial commit

---
+++ sampleTest.java
@@ -0,0 +1,7 @@
+public class sampleTest{
+    public static void main(String [] args){
+System.out.println("Hello world !");
+System.out.println("HI..From Jenkins Project");
+System.out.println("Output from jenkins project");
+    }
+}
